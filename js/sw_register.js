8939d4ee66aab49a9df9bab33f0e92fea1671cf5
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-11-11 10:44:06 -0500

    beautifies JS

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js')
-	.then(function() {
-		console.log('Yay! Your worker is registered!');
-	})
-	.catch(function(err) {
-		console.log('Oops! Your worker registration failed!');
-	});
+    navigator.serviceWorker.register('/sw.js')
+        .then(function() {
+            console.log('Yay! Your worker is registered!');
+        })
+        .catch(function(err) {
+            console.log('Oops! Your worker registration failed!');
+        });
 }
