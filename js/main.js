a9eee54b753a3f05d76e42ce41bf657007bfc81f
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-11-13 00:05:55 -0500

    adds image alt descriptions

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.alt = `${restaurant.name}`;
+    image.alt = `${restaurant.photo_description}`;
     li.append(image);
 
     const name = document.createElement('h1');
